1d745d4aa35a829591b03b37fd977fe9d4aeda9a
심상갑 <kappa2410@gmail.com>
2026-04-22 17:09:49 +0900

    Fix: 시계 눈금 변경 테스트

--- GabHealthKit/SleepDialogView.swift
+++ GabHealthKit/SleepDialogView.swift
@@ -7,14 +7,37 @@
 
 import SwiftUI
 
-enum Clock {
-    enum Hours {
-        case afternoon
+enum ClockHour: Int, CaseIterable {
+    case midnight = 0
+    case twoAM = 2
+    case fourAM = 4
+    case sixAM = 6
+    case eightAM = 8
+    case tenAM = 10
+    case noon = 12
+    case twoPM = 14
+    case fourPM = 16
+    case sixPM = 18
+    case eightPM = 20
+    case tenPM = 22
+
+    var displayText: String {
+        switch self {
+        case .midnight: "오전12시"
+        case .sixAM: "오전6시"
+        case .noon: "오후12시"
+        case .sixPM: "오후6시"
+        case .twoAM, .twoPM: "2"
+        case .fourAM, .fourPM: "4"
+        case .eightAM, .eightPM: "8"
+        case .tenAM, .tenPM: "10"
+        }
     }
 }
 
 struct SleepDialogView: View {
-    let times: [Int] = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22]
+    let times: [ClockHour] = ClockHour.allCases
+    private let dialLabelWidth: CGFloat = 52
     
     var body: some View {
         GeometryReader { proxy in
@@ -28,8 +51,10 @@ struct SleepDialogView: View {
                     Color.blue
                     
                     ForEach(times.indices, id: \.self) { index in
+                        let time = times[index]
+
                         dialTickLabel(
-                            text: "\(times[index])",
+                            text: time.displayText,
                             angle: Double(30 * index),
                             size: dialSize
                         )
@@ -50,16 +75,17 @@ struct SleepDialogView: View {
 
     @ViewBuilder
     private func dialTickLabel(text: String, angle: Double, size: CGFloat) -> some View {
+        let _ = print("상갑 logEvent \(#function) angle \(angle)")
         VStack(spacing: 10) {
-            
             Rectangle()
                 .fill(.white)
                 .frame(width: 3, height: 15)
-            
-            
+
             Text(text)
                 .font(.caption.weight(.semibold))
                 .foregroundStyle(.white)
+                .lineLimit(1)
+                .frame(width: dialLabelWidth)
                 .rotationEffect(.degrees(-angle))
 
             Spacer(minLength: 0)
